b939feee4b47a536322f5cce088e790f0364eb14
melli0505 <dmelli0505@gmail.com>
2023-08-10 12:25:34 +0900

    test with keras rl agent

--- ddpg_test.py
+++ ddpg_test.py
@@ -9,11 +9,13 @@ from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Flatten, Activation, Concatenate, Input
 from tensorflow.keras.optimizers.legacy import Adam
 
-from rl.agents import DDPGAgent
+# from rl.agents import DDPGAgent
 from rl.policy import Policy
 from rl.memory import SequentialMemory
 from rl.random import OrnsteinUhlenbeckProcess
 from rl.processors import MultiInputProcessor
+
+from keras_ddpg import Agent
 
 class ENV(Env):
     def __init__(self):
@@ -41,8 +43,6 @@ class ENV(Env):
         """
         self.state += action -1 
         self.shower_length -= 1 
-        print("state 1: ", self.state)
-        print("action : ", action)
         
         # Calculating the reward
         if self.state[len(self.state)-1] >=37 and self.state[len(self.state)-1] <=39: 
@@ -59,7 +59,7 @@ class ENV(Env):
         # Setting the placeholder for info
         info = {}
         # Returning the step information
-        return self.get_obs(), reward, done, info
+        return self.state, reward, done, info
     
     def get_obs(self):
         observation = np.array([self.state], dtype=np.int8)
@@ -71,13 +71,13 @@ class ENV(Env):
         
         self.state = 38 + random.randint(-5,5)
         self.shower_length = 60 
-        return self.get_obs()
+        return self.state #self.get_obs()
 
 
 def build_actor(env, nb_actions):
     # Next, we build a very simple model.
     actor = Sequential()
-    actor.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+    actor.add(Flatten(input_shape=env.observation_space.shape))
     actor.add(Dense(12))
     actor.add(Activation('relu'))
     actor.add(Dense(12))
@@ -89,8 +89,8 @@ def build_actor(env, nb_actions):
     print(actor.summary())
     return actor
 
-def build_critic(action_input, flattened_observation, observation_input):
-    x = Concatenate()([action_input, flattened_observation])
+def build_critic(action_input, observation_input):
+    x = Concatenate()([action_input, observation_input])
     x = Dense(24)(x)
     x = Activation('relu')(x)
     x = Dense(24)(x)
@@ -105,29 +105,23 @@ def build_critic(action_input, flattened_observation, observation_input):
 
 
 env = ENV()
-
-Adam._name = 'AdamOptimizer'
 nb_actions = env.action_space.n
-print(env.observation_space.shape)
-print((1,) + env.observation_space.shape)
+
 action_input = Input(shape=(nb_actions,), name='action_input')
-observation_input = Input(shape=(1,) + env.observation_space.shape, name='observation_input')
+observation_input = Input(env.observation_space.shape, name='observation_input')
 flattened_observation = Flatten()(observation_input)
-print("action observation flattened: ", action_input.shape, observation_input.shape, flattened_observation.shape)
 
 actor = build_actor(env, nb_actions)
-critic = build_critic(action_input, flattened_observation, observation_input)
-processor = MultiInputProcessor(nb_inputs=5)
-# Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
-# even the metrics!
+critic = build_critic(action_input, observation_input)
+
 memory = SequentialMemory(limit=100000, window_length=1)
 random_process = OrnsteinUhlenbeckProcess(size=nb_actions, theta=.15, mu=0., sigma=.3)
-agent = DDPGAgent(nb_actions=nb_actions, actor=actor, critic=critic, critic_action_input=action_input,
-                  memory=memory, nb_steps_warmup_critic=100, nb_steps_warmup_actor=100,
-                  random_process=random_process, gamma=.99, target_model_update=1e-3, processor=processor)
-agent.compile(Adam(learning_rate=.001, clipnorm=1.), metrics=['mae'])
 
-# Okay, now it's time to learn something! We visualize the training here for show, but this
-# slows down training quite a lot. You can always safely abort the training prematurely using
-# Ctrl + C.
-agent.fit(env, nb_steps=50000, visualize=False, verbose=1, nb_max_episode_steps=200)
+# agent = DDPGAgent(nb_actions=nb_actions, actor=actor, critic=critic, critic_action_input=action_input,
+#                   memory=memory, nb_steps_warmup_critic=10, nb_steps_warmup_actor=10,
+#                   random_process=random_process, gamma=.99, target_model_update=1e-3, is_discrete=True) #, processor=processor)
+# agent = ddpgAgent(env, is_discrete=True, batch_size=12, actor=actor, critic=critic)
+agent = Agent(input_dims=[1, 1], env=env, n_actions=nb_actions, fc1=12, fc2=24)
+
+# agent.compile(Adam(learning_rate=.001, clipnorm=1.), metrics=['mae'])
+# agent.fit(env, nb_steps=50000, visualize=False, verbose=1, nb_max_episode_steps=200)
---
+++ duel_dqn_test.py
@@ -0,0 +1,112 @@
+import numpy as np
+from gym import Env
+from gym.spaces import Box, Discrete
+import random
+
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Activation
+from tensorflow.keras.optimizers.legacy import Adam
+
+from rl.agents import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+
+
+class ENV(Env):
+    def __init__(self):
+        self.action_space = Discrete(5, start=-2)
+        self.observation_space = Box(low=np.array([0]), high=np.array([100]))
+        self.state = 38 + random.randint(-5,5)
+        self.shower_length = 60
+
+    def step(self, action):
+        """
+        Training forward function
+        - 실제 simulation 환경에서 fan 속도 제어 명령을 보낼 곳
+        - reward를 받아올 때까지 시간 delay가 필요함
+
+        Args:
+            action (int, sample): 팬 속도 제어
+
+        Returns:
+            state: _description_
+            reward: action에 따른 보상
+            done: episode 종료 여부 표시
+            info: .
+        """
+        self.state += action -1 
+        self.shower_length -= 1 
+        
+        # Calculating the reward
+        if self.state >=37 and self.state <=39: 
+            reward =1 
+        else: 
+            reward = -1 
+        
+        # Checking if shower is done
+        if self.shower_length <= 0: 
+            done = True
+        else:
+            done = False
+        
+        # Setting the placeholder for info
+        info = {}
+        
+        # Returning the step information
+        return self.state, reward, done, info
+    
+    def reset(self):
+        self.state = 38 + random.randint(-5,5)
+        self.shower_length = 60 
+        return self.state
+
+
+Adam._name = 'AdamOptimizer'
+
+def build_model(states, actions):
+    model = Sequential()    
+    model.add(Dense(24, activation='relu', input_shape=states))
+    model.add(Dense(16))
+    model.add(Activation('relu'))
+    model.add(Dense(16))
+    model.add(Activation('relu'))
+    model.add(Dense(16))
+    model.add(Activation('relu'))
+    model.add(Dense(actions, activation='linear'))
+    return model
+
+def build_agent(model, actions):
+    policy = BoltzmannQPolicy()
+    memory = SequentialMemory(limit=50000, window_length=1)
+    dqn = DQNAgent(model=model, memory=memory, policy=policy, 
+                  nb_actions=actions, nb_steps_warmup=10, enable_dueling_network=True, dueling_type='avg', target_model_update=1e-2)
+    return dqn
+
+
+
+env = ENV()
+
+states = env.observation_space.shape
+actions = env.action_space.n
+
+model = build_model(states, actions)
+print(model.summary())
+
+dqn = build_agent(model, actions)
+dqn.compile(Adam(learning_rate=1e-3), metrics=['mae'])
+dqn.fit(env, nb_steps=200000, visualize=False, verbose=1)
+
+results = dqn.test(env, nb_episodes=150, visualize=False)
+print(np.mean(results.history['episode_reward']))
+# episodes = 20
+# for episode in range(1, episodes + 1):
+#     state = env.reset()
+#     done = False
+#     score = 0
+    
+#     while not done:
+#         action = env.action_space.sample()
+#         next_state, reward, done, info = env.step(action)
+#         score += reward
+#     print(f"Episode: {episode} Score: {score}")dummy_ddpg_test.py
